525706688d3da582f4fbbb1b88adaebfbca2df94
Margarita Loskutova <jazsoul8@gmail.com>
2023-02-18 19:05:21 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  const number = Math.abs(n);
+  const numberDigitsArray = Array.from(String(number).split('').reverse());
+  return numberDigitsArray.join('');
 }
+
